66faa66ddb7b2d5726aaebd44aa450c8879fbb0f
Marcus Lim <markerlim15@gmail.com>
2024-12-14 12:31:28 +0800

    start of project

---
+++ src/main/java/com/project/mini_ssf/MiniSsfApplication.java
@@ -0,0 +1,13 @@
+package com.project.mini_ssf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mini_ssf/MiniSsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mini_ssf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
